4c910e5a3faa6c71d1aee482ae24e44b8687bdab
Jinihl Kim <jinihl@gmail.com>
2011-03-28 11:02:22 +0900

    add other files

---
+++ Test/TestAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  TestAppDelegate.h
+//  Test
+//
+//  Created by MB_04 on 11. 3. 21..
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
